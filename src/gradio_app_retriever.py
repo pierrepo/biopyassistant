5d20e57069cced8dce370797d9b5c69d86d4ece5
uvsq22004047 <essmay.touami@ens.uvsq.fr>
2024-05-21 21:58:58 +0200

    Add the quiz section in gradio and the retriever in Streamlit

--- src/gradio_app_retriever.py
+++ src/gradio_app_retriever.py
@@ -23,19 +23,19 @@ from loguru import logger
 
 
 # MODULES IMPORT
-from query_chatbot_by_retriever import load_database, search_similarity_in_database, get_metadata, format_chat_history, calculate_nb_tokens, contextualize_question,  generate_answer, add_metadata_to_answer, MSGS_QUERY_NOT_RELATED
-
+from query_chatbot_by_retriever import load_database, search_similarity_in_database, get_metadata, format_chat_history, contextualize_question,  generate_answer, add_metadata_to_answer, MSGS_QUERY_NOT_RELATED, OPENAI_MODEL_NAME, CHROMA_PATH
+from create_quiz import create_quiz
+from verify_answer_quiz import generate_feedback
 
 # CONSTANTS
-VECTOR_DB_PATH = "chroma_db"
-OPENAI_MODEL_NAME = "gpt-3.5-turbo"
 FLAVICON_RELATIVE_PATH = 'data/logo_round.ico'
 QUERY_EXAMPLES= [
     ["C'est quoi la différence entre une liste et un set ?"],
     ["Comment on fait une boucle for en Python ?"], 
     ["Qu'est-ce que la récursivité ?"],
 ]
-
+QUIZ_TYPES = ["QCM", "Vrai ou Faux", "Questions ouvertes", "Exercice de code", "Trouver l'erreur dans le code", "Trouver la sortie du code", "Compléter le code"]
+CHAPTERS = ["1.Introduction", "2.Variables", "3.Affichage", "4.Listes", "5.Boucles", "6.Tests", "7.Modules", "8.Fonctions", "9.Chaine de caractères", "10.Dictionnaires", "11.Bonne pratiques", "12.Expressions régulières", "13.Jupyter", "14.Classes", "15.tkinter"]
 
 # FUNCTIONS
 def respond(message: str, chat_history: list, python_level: str) -> str:
@@ -98,7 +98,7 @@ def vote(data: gr.LikeData) -> None: # TODO: Save the votes in a file to do som
         logger.info(f"You downvoted this response: {data.value}\n")
 
 
-def display_python_level(data: gr.LikeData):
+def display_python_level(data: gr.SelectData):
     """Display in the logs the Python level selected by the user.
     
     Parameters
@@ -109,6 +109,94 @@ def display_python_level(data: gr.LikeData):
     logger.info(f"Python level selected: {data.value}\n")
 
 
+def get_user_input(data: gr.SelectData) -> str:
+    """Get the user input.
+    
+    Parameters
+    ----------
+    data : gr.SelectData
+        The data containing the user input.         
+    
+    Returns
+    -------
+    str
+        The user input.
+    """
+    logger.info(f"{data.target.label} {data.value}\n")
+    logger.info(f"Type of the input: {type(data.value)}\n")
+    
+    return data.value
+
+
+def generate_quiz(chapter: str, thematic: str, difficulty: str, quiz_type: str, nb_questions: int) -> str:
+    """Generate a quiz to ask the user.
+    
+    Parameters
+    ----------
+    chapter : str
+        The chapter of the question.
+    thematic : str
+        The thematic of the question.
+    difficulty : str
+        The difficulty of the question.
+    quiz_type : str
+        The type of the question.
+    nb_questions : int
+        The number of questions to generate.
+    answer_placeholder : gr.TextArea
+        The placeholder for the answer.
+    
+    Returns
+    -------
+    str
+        The generated quiz.
+    """
+    logger.info("Generating a quiz.")
+
+    # Display the quiz parameters
+    logger.info(f"Chapter: {chapter}")
+    logger.info(f"Thematic: {thematic}")
+    logger.info(f"Difficulty: {difficulty}")
+    logger.info(f"Quiz type: {quiz_type}")
+    logger.info(f"Number of questions: {nb_questions}")
+    
+    # Create the quiz
+    quiz = create_quiz(chapter=chapter, thematic=thematic, difficulty=difficulty, quiz_type=quiz_type, nb_questions=nb_questions)
+
+    logger.info(f"Quiz generated: {quiz}")
+    logger.success("Quiz generated successfully.\n")
+
+    return quiz
+
+
+def verify_answer(answer: str, quiz: str, quiz_type: str) -> str:
+    """Submit the answer to the quiz.
+    
+    Parameters
+    ----------
+    answer : str
+        The answer to the quiz.
+    quiz : str
+        The quiz to answer.
+    quiz_type : str
+        The type of the quiz.
+    
+    Returns
+    -------
+    str
+        The feedback to the user.
+    """
+    logger.info("Submitting the answer.")
+    
+    # Generate the feedback
+    feedback = generate_feedback(answer=answer, quiz=quiz, quiz_type=quiz_type)
+
+    logger.info(f"Feedback generated: {feedback}")
+    logger.success("Feedback generated successfully.\n")
+
+    return feedback
+
+
 def create_interface():
     """Create the Gradio interface."""
     with gr.Blocks(theme=gr.themes.Default(primary_hue="emerald", secondary_hue="emerald"), title="BioPyAssistant") as demo:
@@ -116,7 +204,7 @@ def create_interface():
         gr.HTML("""<h1 text-align="center" style="font-size: 3em;"><center> 🐍 BioPyAssistant 🐍 </center></h1>""")
         
         # Add a description
-        with gr.Accordion(label="Description du projet :", open=True):
+        with gr.Accordion(label="Description du projet :", open=False):
             gr.HTML("""<p text-align="center" style="font-size: 1em;">
                     Bienvenue sur BioPyAssistant, ton assistant Python pour apprendre à coder en Python. Pose-moi une question sur le cours et je te répondrai !
                     Tu peux aussi me poser des questions sur un chapitre spécifique du cours ou faire un QCM pour tester tes connaissances.
@@ -133,7 +221,6 @@ def create_interface():
             # Define the chatbot
             bot = gr.Chatbot(
                 value  = [["Hey, j'ai besoin d'aide en Python !", "Bonjour, je suis BioPyAssistant, ton assistant pour répondre à tes questions sur Python. Comment puis-je t'aider ?"]], 
-                elem_id="chatbot",
                 bubble_full_width=False,
                 height=300,
                 likeable=True,
@@ -165,16 +252,58 @@ def create_interface():
 
     
         # Add a section for asking a qcm about a specific chapter
-        with gr.Tab("QCM"):
-            pass
+        with gr.Tab("Se tester"):
+            with gr.Row() as main_options :
+                # Add a title
+                gr.Markdown("## Options :")
+            
+                # Define the options
+                chapter = gr.Dropdown(choices=CHAPTERS,label="Choisis un chapitre :", multiselect=True)
 
+                # Define the thematic
+                thematic = gr.Textbox(label="Thématique (facultatif)", placeholder="Ex: Les boucles for", interactive=True)
+
+                # Define the difficulty level
+                difficulty = gr.Dropdown(choices=["Facile", "Intérmediaire", "Difficile", "Expert"], label="Choisis la difficulté :")
+                
+                # Define the quiz type
+                quiz_type = gr.Dropdown(choices=["QCM", "Question ouverte", "Vrai ou Faux", "Exercice de code", "Trouver l'erreur dans le code", "Complète le code", "Trouver la sortie du code"], label="Choisis le type de question :")
+
+                # Define the number of questions
+                nb_questions = gr.Slider(1, 10, step=1, label="Nombre de questions :", interactive=True)
+
+                # Add a button to generate the quiz
+                submit_options = gr.Button("Générer le Quiz", size="sm")
+
+            # Display the options selected
+            chapter.select(get_user_input)   
+            #thematic.submit(get_user_input)   
+            difficulty.select(get_user_input)   
+            quiz_type.select(get_user_input)
+            #nb_questions.input(get_user_input)
+
+            # Define the question textarea
+            quiz_placeholder = gr.TextArea(label="Quiz", lines=2, interactive=False)
+            # Define the response textarea
+            answer_placeholder = gr.TextArea(label="Ta réponse", interactive=True)
+            # Define the submit button
+            submit = gr.Button("Valider")
+            # Define the feedback textarea
+            feedback = gr.TextArea(label="Vérification de la réponse", interactive=False)
+            
+            # Generate the quiz with the selected options
+            submit_options.click(generate_quiz, inputs=[chapter, thematic, difficulty, quiz_type, nb_questions], outputs=[quiz_placeholder])
+        
+            # Verify the answer
+            submit.click(verify_answer, inputs=[answer_placeholder, quiz_placeholder, quiz_type], outputs=[feedback])
+            
     return demo
 
 
 # MAIN PROGRAM
 if __name__ == "__main__":
     # Load the vector database
-    vector_db = load_database(VECTOR_DB_PATH)[0]
+    vector_db = load_database(CHROMA_PATH)[0]
     
     # Create the the Gradio interface
     demo = create_interface()
@@ -185,5 +314,5 @@ if __name__ == "__main__":
 
     # Launch the Gradio interface
     demo.launch(favicon_path=FLAVICON_PATH,
-                 inbrowser=True) # to automatically opens a new tab
+                 inbrowser=True, share = True) # to automatically opens a new tab
     
---
+++ src/streamlit_app_retriever.py
@@ -0,0 +1,197 @@
+"""Streamlit app for chatbot testing.
+
+Usage:
+======
+    streamlit run src/streamlit_app_retriever.py
+
+"""
+
+# METADATA
+__authors__ = ("Pierre Poulain", "Essmay Touami")
+__contact__ = "pierre.poulain@u-paris.fr"
+__copyright__ = "BSD-3 clause"
+__date__ = "2024"
+__version__ = "1.0.0"
+
+
+# LIBRAIRIES IMPORT
+import random
+
+import streamlit as st
+from loguru import logger
+
+
+# MODULES IMPORT
+from query_chatbot_by_retriever import load_database, search_similarity_in_database, get_metadata, format_chat_history, contextualize_question,  generate_answer, add_metadata_to_answer, MSGS_QUERY_NOT_RELATED, OPENAI_MODEL_NAME, CHROMA_PATH
+
+
+# FUNCTIONS
+def create_header() -> None:
+    st.markdown(
+        f"""
+        <div style="padding:20px;border-radius:10px;">
+            <h1 style="color:white;text-align:center;font-family:"Monoton", sans-serif;font-size:5em;">🐍 BioPyAssistant 🐍</h1>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown("---")
+
+
+def insert_multiple_spaces(n: int = 1) -> None:
+    """Insert multiple spaces in the streamlit app."""
+    for _ in range(n):
+        st.sidebar.markdown("")
+
+
+def create_sidebar():
+        """Create the sidebar."""
+        st.sidebar.title("Options avancées")
+
+        # Python proficiency level
+        python_level = st.sidebar.selectbox("Niveau de Maîtrise de Python :", ("Débutant", "Intermédiaire", "Avancé"))
+
+        # Insert multiple spaces
+        insert_multiple_spaces(3)
+
+        # Initialize total cost if not already set
+        if 'total_cost' not in st.session_state:
+            st.session_state.total_cost = 0.0
+
+        # Insert multiple spaces
+        insert_multiple_spaces(3)
+
+        # Clear the conversation
+        if st.sidebar.button("Effacer la conversation"):
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Bonjour, je suis BioPyAssistant, ton assistant pour répondre à tes questions sur Python. Comment puis-je t'aider ?"}
+            ]
+            st.session_state.total_cost = 0.0
+
+        return python_level
+
+
+def transform_messages(messages: list[dict]) -> list[tuple]:
+    """Transform the messages into a list of tuples.
+
+    Parameters
+    ----------
+    messages : list[dict]
+        The chat history messages.
+    
+    Returns
+    -------
+    list[tuple]
+        The chat history messages as a list of tuples.
+    """
+    logger.info("Transforming the messages into a list of tuples.")
+    messages_tuples = []
+    user_message = None
+
+    # Ignorer le premier message automatique de l'assistant
+    start_index = 1 if messages[0]['role'] == 'assistant' else 0
+    
+    for message in messages[start_index:]:
+        if message['role'] == 'user':
+            user_message = message['content']
+        elif message['role'] == 'assistant' and user_message is not None:
+            messages_tuples.append((user_message, message['content']))
+            user_message = None  # Réinitialiser le message de l'utilisateur après avoir créé le tuple
+    
+    return messages_tuples
+
+
+def generate_response(user_query: str, vector_db, python_level: str) -> str:
+    """Generate a response to the user question.
+    
+    Parameters
+    ----------
+    user_query : str
+        The user question.
+    python_level : str
+        The Python level of the user.
+
+    Returns
+    -------
+    response : str
+        The response predicted by the model.
+    """
+    # Transform the chat history into a list of tuples
+    chat_history = transform_messages(st.session_state.messages)
+    # Format the chat history for the model
+    formatted_chat_history = format_chat_history(chat_history, len_history=10)
+
+    # Contextualize the user question with the chat history
+    query_contextualized = contextualize_question(user_query=user_query, chat_history_formatted=formatted_chat_history, model_name=OPENAI_MODEL_NAME)
+    # Search for relevant documents in the database
+    context = search_similarity_in_database(vector_db=vector_db, user_query=query_contextualized)
+    
+    # If no relevant document was found
+    if context == []:
+        logger.info("No relevant documents found in the database.")
+        logger.success("Returning an automatic response without calling the model.")
+        # random response betweet responses in MSGS_QUERY_NOT_RELATED
+        response = random.choice(MSGS_QUERY_NOT_RELATED)
+        return response
+    else :
+        # Get the metadata of the relevant documents
+        metadata = get_metadata(context)
+        # Generate the answer
+        answer = generate_answer(query_contextualized=query_contextualized, relevant_chunks=context, model_name=OPENAI_MODEL_NAME, python_level=python_level)
+        # Add metadata to the answer
+        final_answer = add_metadata_to_answer(answer, metadatas=metadata, iu=True)
+
+        logger.info(f"Response generated: {final_answer}")
+        logger.success("Response generated successfully.\n")
+                    
+        return final_answer
+
+
+def chat_with_bot(vector_db, python_level) -> None:
+    """Chat with the bot"""
+    # Initialize the chat with a welcome message
+    if "messages" not in st.session_state.keys(): # Initialize the chat message history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Bonjour, je suis BioPyAssistant, ton assistant pour répondre à tes questions sur Python. Comment puis-je t'aider ?"}
+        ]
+
+    # Get the user question
+    if user_query := st.chat_input("Pose moi une question sur le cours !"): # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": user_query})
+
+    for message in st.session_state.messages: # Display the prior chat messages
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("En réflexion..."):
+                response = generate_response(user_query, vector_db, python_level)
+                st.write(response)
+
+                # Add the response to the chat history
+                message = {"role": "assistant", "content": response}
+                st.session_state.messages.append(message)
+
+
+def main():
+    # Set the page configuration
+    st.set_page_config(page_title="BioPyAssistant", page_icon="data/logo_round.webp")
+
+    # Create the header
+    create_header()
+    
+    # Create a sidebar and get the user inputs
+    python_level = create_sidebar()
+
+    # Load the vector database
+    vector_db = load_database(CHROMA_PATH)[0]
+
+    # Chat with the bot
+    chat_with_bot(vector_db, python_level)
+
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    main()
